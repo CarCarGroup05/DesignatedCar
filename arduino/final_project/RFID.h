7ce818d77ffa1a8240d9969e213767a71c3d9cb8
ChenBingWei1201 <rain4030633@gmail.com>
2023-04-15 16:39:41 +0800

    modified rfid.h

--- arduino/final_project/RFID.h
+++ arduino/final_project/RFID.h
@@ -14,15 +14,21 @@
 
 byte* rfid(byte& idSize) {
     // 確認是否有新卡片
+    if(!mfrc522.PICC_IsNewCardPresent()) {
+      goto FuncEnd;
+    } //PICC_IsNewCardPresent()：是否感應到新的卡片?
+    if(!mfrc522.PICC_ReadCardSerial()) {
+      goto FuncEnd;
+    }
     if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
       byte *id = mfrc522.uid.uidByte;   // 取得卡片的UID
       idSize = mfrc522.uid.size;   // 取得UID的長度
 
-      //Serial.print("PICC type: ");      // 顯示卡片類型
+      // Serial.print("PICC type: ");      // 顯示卡片類型
       // 根據卡片回應的SAK值（mfrc522.uid.sak）判斷卡片類型
-      //MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
-      //Serial.println(mfrc522.PICC_GetTypeName(piccType));
-      #ifdef DEBUG
+      // MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
+      // Serial.println(mfrc522.PICC_GetTypeName(piccType));
+      // #ifdef DEBUG
       Serial.print("UID Size: ");       // 顯示卡片的UID長度值
       Serial.println(idSize);   
       for (byte i = 0; i < idSize; i++) {  // 逐一顯示UID碼
@@ -32,8 +38,10 @@ byte* rfid(byte& idSize) {
         Serial.println(id[i], HEX);       // 以16進位顯示UID值  
       }
       Serial.println();
-      #endif
+      // #endif
+      
       mfrc522.PICC_HaltA();  // 讓卡片進入停止模式
+      FuncEnd:; // goto 跳到這.
       return id; // return type : byte*
     }
     return 0;
