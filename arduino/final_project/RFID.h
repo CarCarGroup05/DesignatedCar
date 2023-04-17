24249dda5d6dd5f33c8117f39250db065381f483
timtimding <ppdjh2240@gmail.com>
2023-04-18 00:07:35 +0800

    RFID issue SOLVED!

--- arduino/final_project/RFID.h
+++ arduino/final_project/RFID.h
@@ -12,9 +12,10 @@
 #include <MFRC522.h>     // 引用程式庫
 /* pin---- SDA:9 SCK:13 MOSI:11 MISO:12 GND:GND RST:define on your own  */
 
-byte* rfid(byte& idSize) {
+byte* rfid(byte& idSize, bool& newlyFound) {
     // 確認是否有新卡片
     if(!mfrc522.PICC_IsNewCardPresent()) {
+      newlyFound = true;
       goto FuncEnd;
     } //PICC_IsNewCardPresent()：是否感應到新的卡片?
     if(!mfrc522.PICC_ReadCardSerial()) {
--- arduino/final_project/final_project.ino
+++ arduino/final_project/final_project.ino
@@ -2,81 +2,75 @@
 // File       [final_project.ino]
 // Author     [Erik Kuo]
 // Synopsis   [Code for managing main process]
-// Functions  [setup, loop, Search_Mode, Hault_Mode, SetState]
+// Functions  [setup, loop, Search, getPath]
 // Modify     [2020/03/27 Erik Kuo]
 /***************************************************************************/
 
-#define DEBUG // debug flag
-
-// for RFID
+#define DEBUG  // debug flag
 #include <SPI.h>
 #include <MFRC522.h>
 #include <SoftwareSerial.h>
-SoftwareSerial BT (10, 11);
+#include "node.h"
+
 /*===========================define pin & create module object================================*/
 // BlueTooth
-// BT connect to Serial1 (Hardware Serial)
-// Mega               HC05
-// Pin  (Function)    Pin
-// 10    TX       ->  RX
-// 11    RX       <-  TX
+// Mega          HC05
+// Pin (Function) Pin
+// 10    TX   ->   RX
+// 11    RX   <-   TX
+SoftwareSerial BT(10, 11);
 
 // 馬達控制器(TB6612), 請按照自己車上的接線寫入腳位(左右不一定要跟註解寫的一樣)
-#define MotorR_I1      2 //定義 A1 接腳（右）
-#define MotorR_I2      3 //定義 A2 接腳（右）
-#define MotorR_PWMR    9//定義 ENA (PWM調速) 接腳
-#define MotorL_I3      5 //定義 B1 接腳（左）
-#define MotorL_I4      6 //定義 B2 接腳（左）
-#define MotorL_PWML    8//定義 ENB (PWM調速) 接腳
+#define MotorR_I1 2    //定義 A1 接腳（右）
+#define MotorR_I2 3    //定義 A2 接腳（右）
+#define MotorR_PWMR 9  //定義 ENA (PWM調速) 接腳
+#define MotorL_I3 5    //定義 B1 接腳（左）
+#define MotorL_I4 6    //定義 B2 接腳（左）
+#define MotorL_PWML 8  //定義 ENB (PWM調速) 接腳
+
 // 紅外線模組(IR模組), 請按照自己車上的接線寫入腳位
 #define IRpin_LL 32
-#define IRpin_L  34
-#define IRpin_M  36
-#define IRpin_R  38
+#define IRpin_L 34
+#define IRpin_M 36
+#define IRpin_R 38
 #define IRpin_RR 40
+
 // RFID, 請按照自己車上的接線寫入腳位
-#define RST_PIN      7        // 讀卡機的重置腳位
-#define SS_PIN       53       // 晶片選擇腳位
+#define RST_PIN 7                  // 讀卡機的重置腳位
+#define SS_PIN 53                  // 晶片選擇腳位
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // 建立MFRC522物件
 /*===========================define pin & create module object===========================*/
-int mapState = 0;
-bool start = false;
-bool received = false;
-char inp[10];
-char treasureMap[100];
-int irRead = 0;
+
 /*============setup============*/
 // MFRC522 *mfrc522;
 // 宣告MFRC522指標
-void setup()
-{
-   //bluetooth initialization
-   BT.begin(9600);
-   //Serial window
-   Serial.begin(9600);
-
+void setup(){
+  //bluetooth initialization
+  BT.begin(9600);
+  //Serial window
+  Serial.begin(9600);
   //RFID initial
-   SPI.begin();
-   mfrc522.PCD_Init();
+  SPI.begin();
+  mfrc522.PCD_Init();
 
-   //TB6612(motor) pin
-   pinMode(MotorR_I1,   OUTPUT);
-   pinMode(MotorR_I2,   OUTPUT);
-   pinMode(MotorL_I3,   OUTPUT);
-   pinMode(MotorL_I4,   OUTPUT);
-   pinMode(MotorL_PWML, OUTPUT);
-   pinMode(MotorR_PWMR, OUTPUT);
+  //TB6612(motor) pin
+  pinMode(MotorR_I1, OUTPUT);
+  pinMode(MotorR_I2, OUTPUT);
+  pinMode(MotorL_I3, OUTPUT);
+  pinMode(MotorL_I4, OUTPUT);
+  pinMode(MotorL_PWML, OUTPUT);
+  pinMode(MotorR_PWMR, OUTPUT);
 
-   //tracking(IR) pin
-   pinMode(IRpin_LL, INPUT); 
-   pinMode(IRpin_L, INPUT);
-   pinMode(IRpin_M, INPUT);
-   pinMode(IRpin_R, INPUT);
-   pinMode(IRpin_RR, INPUT);
+  //tracking(IR) pin
+  pinMode(IRpin_LL, INPUT);
+  pinMode(IRpin_L, INPUT);
+  pinMode(IRpin_M, INPUT);
+  pinMode(IRpin_R, INPUT);
+  pinMode(IRpin_RR, INPUT);
 #ifdef DEBUG
   Serial.println("Start!");
+  done();
 #endif
-
 }
 /*============setup============*/
 
@@ -84,19 +78,21 @@ void setup()
 #include "RFID.h"
 #include "track.h"
 #include "bluetooth.h"
-#include "node.h"
 /*=====Import header files=====*/
 
 /*===========================initialize variables===========================*/
-int l2=0,l1=0,m0=0,r1=0,r2=0; //紅外線模組的讀值(0->white,1->black)
-int _Tp=160; //set your own value for motor power
-bool state=false; //set state to false to halt the car, set state to true to activate the car
-BT_CMD _cmd = S; //enum for bluetooth message, reference in bluetooth.h line 2
-// BT_CMD _cmd = NOTHING; //enum for bluetooth message, reference in bluetooth.h line 2
+int _Tp = 160;  // set your own value for motor power
+int mapState = 0;
+bool start = false;
+bool received = false;
+char treasureMap[256];
+int irRead = 0;
+byte idSize = 8;
+bool newlyFound = false; // flag set as true when find a new TREASURE
 /*===========================initialize variables===========================*/
 
 /*===========================declare function prototypes===========================*/
-void Search();// search graph
+void Search();  // search graph
 void getPath(char tMap);
 /*===========================declare function prototypes===========================*/
 
@@ -112,69 +108,31 @@ void loop(){
     if(askStart()){
       BT.write("Start~");
       start = true;
-      switch(treasureMap[0]){
-        case 'S':
-           MotorWriting(0, 0);
-          break;
-        case 'M':
-          MotorWriting(_Tp, _Tp);
-          delay(1000);
-          break;
-        case 'R':
-          lrTurn(1);
-          break;
-        case 'L':
-          lrTurn(0);
-          break;
-        case 'B':
-          backTurn();
-          break;
-        default:
-           break;
-      }
+      motionSwitch(treasureMap[0]);
       mapState++;
     }
   }
-  Search(); // car can start search
-int mapState = 0; //
-/*===========================define function===========================*/
-
-char treasureMap[256];
-// bool detected = false;
-
-void SetState();
-bool detected = false;
-
-void loop()
-{
-   if(!state) MotorWriting(0,0); // hault the car
-   else Search(); // car can start search
-   SetState(); // 
-   getPath(treasureMap);
-   
-   byte idSize = 8;
-   send_byte(rfid(idSize)/*, idSize*/); 
+  Search();
+  send_byte(rfid(idSize, newlyFound) /*, idSize*/, newlyFound);
+  // flag newlyFound determines whether to send UID
 }
 
-void Search()
-{
-  // TODO: let your car search graph(maze) according to bluetooth command from computer(python code)
+/*===========================define function===========================*/
+void Search(){
   if(tracking(treasureMap[mapState]))
     mapState++;
-
-
-  
   if(mapState >= strlen(treasureMap)){
     MotorWriting(0, 0);
     BT.write("done!");
+    successs();
+    start = false;
   }
+}
 
+void getPath(char tMap){
+  if(BT.available()) {
+    BT.readBytes(&tMap, 256);
+    Serial.println(tMap);
+  }
 }
 /*===========================define function===========================*/
-void getPath(char tMap){
-  if(BT.available()){
-      BT.readBytes(&tMap, 256);
-      Serial.println(tMap);
-  }
-}
-// BingWei success push!
