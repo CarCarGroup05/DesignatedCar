6dd6c007de3a40d752dd601c15783405e54d7d84
ChenBingWei1201 <rain4030633@gmail.com>
2023-04-25 17:26:04 +0800

    modified main.py

--- python/main.py
+++ python/main.py
@@ -13,9 +13,7 @@ def main():
     maze = mz.Maze("data/small_maze.csv")
 
     started = False
-    point = Scoreboard("1WeakGod", "http://140.112.175.18:3000")
-    point = ScoreboardFake("WeakGod", "data/fakeUID.csv")
-    
+
     '''
     interf = BTinterface()
     interf.start
@@ -26,15 +24,24 @@ def main():
     maze.BFS_2(s, e)
     maze.getActions_2()
     interf = BTinterface()
-    interf.send_action(maze.actions_to_str_2())
+    if keyboard.read_key() == "s":
+        interf.send_action(maze.actions_to_str_2())
     print(maze.actions_to_str_2())
-    
+
     while started == False:
         if keyboard.read_key() == "q":
             interf.ser.SerialWriteString("Q")
+            # point = Scoreboard("1WeakGod", "http://140.112.175.18:3000")
+            point = ScoreboardFake("WeakGod", "data/fakeUID.csv")
             started = True
     while True:
-        interf.get_UID()
+        RFID = interf.get_UID()
+        if RFID:
+            point.add_UID(RFID[-8:].upper())
+            print("score:", point.getCurrentScore())
+
+        # interf.ser.SerialReadString()
+
 
 '''
     if (sys.argv[1] == '0'):
