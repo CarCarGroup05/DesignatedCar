cbf59d18354db5dfbfcc5990a8848278b087beac
mark921225 <131502922+mark921225@users.noreply.github.com>
2023-04-29 11:29:21 +0800

    Update main.py

--- python/main.py
+++ python/main.py
@@ -8,43 +8,51 @@ import time
 import sys
 import os
 
-
 def main():
-
-    maze = mz.Maze("data/cross.csv")
-
+    maze = mz.Maze("data/medium_maze.csv")
+    
     started = False
-    # point = Scoreboard("WeakGod", "http://140.112.175.18:3000")
-    # point = ScoreboardFake("WeakGod", "data/fakeUID.csv")
+
+
+    
     '''
     interf = BTinterface()
     interf.start
     '''
     # TODO : Initialize necessary variables
+    '''
     s = int(input("start:"))
     e = int(input("end:"))
-    maze.BFS_2(s, e)
+    maze.BFS_2(s,e)
     maze.getActions_2()
+    '''
+    maze.BFS_continuous(1,100)
+
     interf = BTinterface()
-    interf.send_action(maze.actions_to_str_2())
-    print(maze.actions_to_str_2())
-
-
-    while True:
+    print(maze.shortest_route)
+    if keyboard.read_key() == "s":
+        interf.send_action(maze.actions_to_str())#("BBBBBBBBBBBBBB")
+    print(maze.actions_to_str())
+    
+    while started == False:
         if keyboard.read_key() == "q":
+            point = Scoreboard("WeakGod2", "http://140.112.175.18:3000")
+            #point = ScoreboardFake("WeakGod", "data/fakeUID.csv")
             interf.ser.SerialWriteString("Q")
-            point = Scoreboard("WeakGod", "http://140.112.175.18:3000")
-            # point = ScoreboardFake("WeakGod", "data/fakeUID.csv")
+
             started = True
     while True:
         RFID = interf.get_UID()
+        
         if RFID:
+            print(RFID)
             point.add_UID(RFID[-8:].upper())
-            print("score:", point.getCurrentScore())
+            print("score:",point.getCurrentScore())
 
-         interf.ser.SerialReadString()
+        #interf.ser.SerialReadString()
 
-
+        
+'''
     if (sys.argv[1] == '0'):
         print("Mode 0: for treasure-hunting")
         # TODO : for treasure-hunting, which encourages you to hunt as many scores as possible
@@ -56,4 +64,3 @@ def main():
 if __name__ == '__main__':
     main()
 
-'''
